eba774249a509709dcd47668052dcad740c865d4
Max Eden <mx.eden@gmail.com>
2025-03-13 23:51:46 +0400

    more responsive input field;

--- TestProgram/TestApi/Inspector.cs
+++ TestProgram/TestApi/Inspector.cs
@@ -158,6 +158,10 @@ public class Inspector
 
         foreach (var methodInfo in target.GetType().GetMethods())
         {
+            if (!methodInfo.IsPublic) continue;
+            if (methodInfo.Name.StartsWith("get_")) continue;
+            if (methodInfo.Name.StartsWith("set_")) continue;
+
             if (methodInfo.ReturnType == typeof(void))
             {
                 var row1 = new Rect(area.X, area.Y + height * i, area.Width, height);
@@ -208,18 +212,24 @@ public class Inspector
         var over = area.Contains(pointer);
         var clicked = over && input.DefaultPointer.State == Input.PointerState.GoingDown;
 
-        if (focused) blitter.DrawRect(area, Color32.gray);
-        else blitter.DrawRect(area, Color32.green, CustomRect.Hoverable, Color32.blue);
+        void DrawText()
+        {
+            var focused = _focusedId == _stateId;
 
-        if (focused)
-        {
-            GetSelection(out var start, out var end);
-            blitter.DrawSelection(area, _inputBuffer, start, end, _caretPos);
+            if (focused) blitter.DrawRect(area, Color32.gray);
+            else blitter.DrawRect(area, Color32.green, CustomRect.Hoverable, Color32.blue);
+
+            if (focused)
+            {
+                GetSelection(out var start, out var end);
+                blitter.DrawSelection(area, _inputBuffer, start, end, _caretPos);
+            }
+            else
+            {
+                blitter.DrawText(area, value);
+            }
         }
-        else
-        {
-            blitter.DrawText(area, value);
-        }
+
 
         if (clicked && !focused)
         {
@@ -237,174 +247,182 @@ public class Inspector
                 _caretPos = value.Length;
             }
 
-            return false;
-        }
-
-        if (!focused) return false;
-
-        if (clicked)
-        {
-            _caretPos = blitter.GetCaretPos(area, _inputBuffer, pointer);
-            if (_caretPos >= 0)
-            {
-                _selectionFist = _caretPos;
-                _selectionSecond = _caretPos;
-                _selectionInProgress = true;
-            }
-        }
-
-        if (input.DefaultPointer.State == Input.PointerState.Down && _selectionInProgress)
-        {
-            _caretPos = blitter.GetCaretPos(area, _inputBuffer, pointer);
-
-            if (_selectionFist >= 0)
-            {
-                _selectionSecond = _caretPos;
-            }
-        }
-
-        if (input.DefaultPointer.State == Input.PointerState.GoingUp)
-        {
-            _selectionInProgress = false;
-        }
-
-        if (input.InputTextEvent == Input.TextEventType.Entered && input.InputText != string.Empty)
-        {
-            if (string.IsNullOrEmpty(_inputBuffer))
-            {
-                _inputBuffer = input.InputText;
-                _caretPos = _inputBuffer.Length;
-            }
-            else if (GetSelection(out var start, out var end))
-            {
-                var result = _inputBuffer.Remove(start, end - start);
-                result = result.Insert(start, input.InputText);
-                _caretPos = start + input.InputText.Length;
-                _inputBuffer = result;
-                ClearSelection();
-            }
-            else if (_caretPos >= 0 && _caretPos <= _inputBuffer.Length)
-            {
-                _inputBuffer = _inputBuffer.Insert(_caretPos, input.InputText);
-                _caretPos += input.InputText.Length;
-            }
-
+            DrawText();
             return true;
         }
 
-        if (string.IsNullOrEmpty(_inputBuffer)) return false;
-
-        if (input.InputTextEvent == Input.TextEventType.Backspaced)
+        if (focused)
         {
-            if (GetSelection(out var start, out var end))
+            if (clicked)
             {
-                _inputBuffer = _inputBuffer.Remove(start, end - start);
-                _caretPos = start;
-                ClearSelection();
-            }
-            else if (_caretPos > 0)
-            {
-                _inputBuffer = _inputBuffer.Remove(_caretPos - 1, 1);
-                _caretPos--;
-            }
-
-            return true;
-        }
-        else if (input.InputTextEvent == Input.TextEventType.Deleted)
-        {
-            if (GetSelection(out var start, out var end))
-            {
-                _inputBuffer = _inputBuffer.Remove(start, end - start);
-                _caretPos = start;
-                ClearSelection();
-            }
-            else if (_caretPos < _inputBuffer.Length)
-            {
-                _inputBuffer = _inputBuffer.Remove(_caretPos, 1);
-                _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
-            }
-
-            return true;
-        }
-        else if (input.InputTextEvent == Input.TextEventType.Left)
-        {
-            if (input.InputShift)
-            {
-                if (_caretPos > 0)
+                _caretPos = blitter.GetCaretPos(area, _inputBuffer, pointer);
+                if (_caretPos >= 0)
                 {
-                    if (!_selectionInProgress)
+                    _selectionFist = _caretPos;
+                    _selectionSecond = _caretPos;
+                    _selectionInProgress = true;
+                }
+            }
+
+            if (input.DefaultPointer.State == Input.PointerState.Down && _selectionInProgress)
+            {
+                _caretPos = blitter.GetCaretPos(area, _inputBuffer, pointer);
+
+                if (_selectionFist >= 0)
+                {
+                    _selectionSecond = _caretPos;
+                }
+            }
+
+            if (input.DefaultPointer.State == Input.PointerState.GoingUp)
+            {
+                _selectionInProgress = false;
+            }
+
+            if (input.InputTextEvent == Input.TextEventType.Entered && input.InputText != string.Empty)
+            {
+                if (string.IsNullOrEmpty(_inputBuffer))
+                {
+                    _inputBuffer = input.InputText;
+                    _caretPos = _inputBuffer.Length;
+                }
+                else if (GetSelection(out var start, out var end))
+                {
+                    var result = _inputBuffer.Remove(start, end - start);
+                    result = result.Insert(start, input.InputText);
+                    _caretPos = start + input.InputText.Length;
+                    _inputBuffer = result;
+                    ClearSelection();
+                }
+                else if (_caretPos >= 0 && _caretPos <= _inputBuffer.Length)
+                {
+                    _inputBuffer = _inputBuffer.Insert(_caretPos, input.InputText);
+                    _caretPos += input.InputText.Length;
+                }
+
+                DrawText();
+                return true;
+            }
+
+            if (!string.IsNullOrEmpty(_inputBuffer))
+            {
+                if (input.InputTextEvent == Input.TextEventType.Backspaced)
+                {
+                    if (GetSelection(out var start, out var end))
                     {
-                        _selectionInProgress = true;
-                        _selectionFist = _caretPos;
-                        _selectionSecond = _caretPos - 1;
+                        _inputBuffer = _inputBuffer.Remove(start, end - start);
+                        _caretPos = start;
+                        ClearSelection();
+                    }
+                    else if (_caretPos > 0)
+                    {
+                        _inputBuffer = _inputBuffer.Remove(_caretPos - 1, 1);
+                        _caretPos--;
+                    }
+
+                    DrawText();
+                    return true;
+                }
+                else if (input.InputTextEvent == Input.TextEventType.Deleted)
+                {
+                    if (GetSelection(out var start, out var end))
+                    {
+                        _inputBuffer = _inputBuffer.Remove(start, end - start);
+                        _caretPos = start;
+                        ClearSelection();
+                    }
+                    else if (_caretPos < _inputBuffer.Length)
+                    {
+                        _inputBuffer = _inputBuffer.Remove(_caretPos, 1);
+                        _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
+                    }
+
+                    DrawText();
+                    return true;
+                }
+                else if (input.InputTextEvent == Input.TextEventType.Left)
+                {
+                    if (input.InputShift)
+                    {
+                        if (_caretPos > 0)
+                        {
+                            if (!_selectionInProgress)
+                            {
+                                _selectionInProgress = true;
+                                _selectionFist = _caretPos;
+                                _selectionSecond = _caretPos - 1;
+                            }
+                            else
+                            {
+                                _selectionSecond = _caretPos - 1;
+                            }
+
+                            _caretPos--;
+                        }
+                    }
+                    else if (GetSelection(out int start, out int end))
+                    {
+                        _caretPos = start;
+                        ClearSelection();
                     }
                     else
                     {
-                        _selectionSecond = _caretPos - 1;
+                        _caretPos--;
+                        _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
                     }
-
-                    _caretPos--;
                 }
-            }
-            else if (GetSelection(out int start, out int end))
-            {
-                _caretPos = start;
-                ClearSelection();
-            }
-            else
-            {
-                _caretPos--;
-                _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
-            }
-        }
-        else if (input.InputTextEvent == Input.TextEventType.Right)
-        {
-            if (input.InputShift)
-            {
-                if (_caretPos < _inputBuffer.Length)
+                else if (input.InputTextEvent == Input.TextEventType.Right)
                 {
-                    if (!_selectionInProgress)
+                    if (input.InputShift)
                     {
-                        _selectionInProgress = true;
-                        _selectionFist = _caretPos;
-                        _selectionSecond = _caretPos + 1;
+                        if (_caretPos < _inputBuffer.Length)
+                        {
+                            if (!_selectionInProgress)
+                            {
+                                _selectionInProgress = true;
+                                _selectionFist = _caretPos;
+                                _selectionSecond = _caretPos + 1;
+                            }
+                            else
+                            {
+                                _selectionSecond = _caretPos + 1;
+                            }
+
+                            _caretPos++;
+                        }
+                    }
+                    else if (GetSelection(out int start, out int end))
+                    {
+                        _caretPos = end;
+                        ClearSelection();
                     }
                     else
                     {
-                        _selectionSecond = _caretPos + 1;
+                        _caretPos++;
+                        _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
                     }
-
-                    _caretPos++;
                 }
-            }
-            else if (GetSelection(out int start, out int end))
-            {
-                _caretPos = end;
-                ClearSelection();
-            }
-            else
-            {
-                _caretPos++;
-                _caretPos = Math.Clamp(_caretPos, 0, _inputBuffer.Length);
-            }
-        }
-        else if (input.InputTextEvent == Input.TextEventType.EnterPressed)
-        {
-            _focusedId = 0;
-            return false;
-        }
-        else if (input.InputTextEvent == Input.TextEventType.Copy)
-        {
-            if (GetSelection(out int start, out int end))
-            {
-                var str = _inputBuffer.Substring(start, end - start);
-                if (!string.IsNullOrEmpty(str))
+                else if (input.InputTextEvent == Input.TextEventType.EnterPressed)
                 {
-                    input.SetToClipboard?.Invoke(str);
+                    _focusedId = 0;
+                    DrawText();
+                    return false;
+                }
+                else if (input.InputTextEvent == Input.TextEventType.Copy)
+                {
+                    if (GetSelection(out int start, out int end))
+                    {
+                        var str = _inputBuffer.Substring(start, end - start);
+                        if (!string.IsNullOrEmpty(str))
+                        {
+                            input.SetToClipboard?.Invoke(str);
+                        }
+                    }
                 }
             }
         }
 
+        DrawText();
         return false;
     }
 }
